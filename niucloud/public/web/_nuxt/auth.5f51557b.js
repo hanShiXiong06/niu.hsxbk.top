f85d6ea0f2f0a99106c9491af15df0bf8f6423f5
全栈小学生 <1518079521@qq.com>
2023-06-21 17:04:47 +0800

    update web

---
+++ niucloud/public/web/_nuxt/auth.5f51557b.js
@@ -0,0 +1,1 @@
+import{cc as e,b3 as o,a6 as t}from"./entry.ba885acf.js";import{u as i}from"./useLogin.e384ae17.js";const u=e((a,r)=>{if(!o())return i().setLoginBack(a),t("/auth/login")});export{u as default};
