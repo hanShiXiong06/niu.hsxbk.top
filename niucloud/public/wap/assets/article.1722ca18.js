6c2cff500cfdeb99c015074a1c1e03d1d9e1397b
全栈小学生 <1518079521@qq.com>
2023-08-12 16:23:53 +0800

    update wap

--- niucloud/public/wap/assets/article.1722ca18.js
+++
@@ -1,1 +0,0 @@
-import{aL as t}from"./index-5bb08350.js";function r(r){return t.get("article/article",r)}function e(r){return t.get("article/article/all",r)}function a(r){return t.get(`article/article/${r}`)}function c(){return t.get("article/category")}export{r as a,a as b,e as c,c as g};
--- niucloud/public/wap/assets/pay.4e07eed1.js
+++
@@ -1,1 +0,0 @@
-import{aL as r}from"./index-5bb08350.js";function o(o){return r.post("pay",o,{showErrorMessage:!0})}function s(o){return r.get(`pay/info/${o}`,{},{showErrorMessage:!0})}export{s as g,o as p};
