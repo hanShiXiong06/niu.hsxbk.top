8061f6d8e9aeea8a8c796b449a6fdd60fa2af8d4
全栈小学生 <1518079521@qq.com>
2023-09-19 11:13:56 +0800

    update wap

---
+++ niucloud/public/wap/assets/pay.5ff55654.js
@@ -0,0 +1,1 @@
+import{b0 as r}from"./index-f4365f98.js";function o(o){return r.post("pay",o,{showErrorMessage:!0})}function s(o,s){return r.get(`pay/info/${o}/${s}`,{},{showErrorMessage:!0})}export{s as g,o as p};
