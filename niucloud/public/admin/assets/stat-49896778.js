5f70a0c4b9c45d623ed89aef6548209ecc0189d2
全栈小学生 <1518079521@qq.com>
2023-10-23 19:26:32 +0800

    update admin code

---
+++ niucloud/public/admin/assets/stat-49896778.js
@@ -0,0 +1,1 @@
+import{A as t}from"./index-981b0207.js";function r(){return t.get("stat/siteindex")}export{r as g};
--- niucloud/public/admin/assets/stat-8df21f7a.js
+++
@@ -1,1 +0,0 @@
-import{A as t}from"./index-80fd3793.js";function r(){return t.get("stat/siteindex")}export{r as g};
